266c432642eeb5dc8d691f3d5604ddff68d6795a
Tomas <tomasignaciocontreras@hotmail.com>
2022-03-28 10:54:24 -0300

    initialized nest repo with donations resource and schema first approach

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql']
+    }),
+    DonationsModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
